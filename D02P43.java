89649141a2553019c212041f302186e197b68f5f
Susmitha Pasumarthi <80407639+SusmithaPasumarthi-511@users.noreply.github.com>
2024-04-04 09:48:30 +0530

    Add files via upload
    Day1Session1 Assignment Files

---
+++ D02P43.java
@@ -0,0 +1,32 @@
+package com.learning.core.day1session1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class D02P43 {
+
+	public static int findFirstRepeatingIndex(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                return i;
+            } else {
+                map.put(arr[i], i);
+            }
+        }
+        
+        return -1; // If no repeating element found
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {7, 10, 5, 3, 4, 3, 5, 6};
+        int index = findFirstRepeatingIndex(arr);
+        
+        if (index != -1) {
+            System.out.println("Index of the first repeating element: " + index);
+        } else {
+            System.out.println("No repeating element found.");
+        }
+    }
+}

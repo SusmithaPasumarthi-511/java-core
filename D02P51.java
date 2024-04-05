cb87c01469c617a2ceb4a7b552d2b6dbb954b40d
Susmitha Pasumarthi <80407639+SusmithaPasumarthi-511@users.noreply.github.com>
2024-04-05 11:07:35 +0530

    Update D02P51.java

--- D02P51.java
+++ D02P51.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class D02P51 {
 
     public static void main(String[] args) {
-        // Taking input from the user
+       
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String inputString = scanner.nextLine();
         scanner.close();
         
-        // Displaying the length of the string
+     
         int length = inputString.length();
         System.out.println("Length of the string: " + length);
         
-        // Converting the string to uppercase
+       
         String upperCaseString = inputString.toUpperCase();
         System.out.println("String in uppercase: " + upperCaseString);
         
-        // Checking if the string is a palindrome
+     
         boolean isPalindrome = checkPalindrome(inputString);
         if (isPalindrome) {
             System.out.println("The string is a palindrome.");
@@ -28,7 +28,6 @@ public class D02P51 {
         }
     }
     
-    // Function to check if a string is a palindrome
     public static boolean checkPalindrome(String str) {
         int i = 0, j = str.length() - 1;
         while (i < j) {
